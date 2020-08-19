10bf6ddaefe3ab7928d1ab49114d622d4faeb502
Vladyslav Asaievych <hephest1904@gmail.com>
2020-08-19 14:19:30 +0300

    Added utils.py & tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+import os
+from unittest import TestCase
 
-# Create your tests here.
+from .utils import (
+    api_get_images_id, api_get_images_pages_count, api_get_token,
+    build_images_urls,
+)
+
+API_KEY = os.environ['API_KEY']
+
+
+class GalleryUtilsTest(TestCase):
+
+    def test_utils_api_get_token_works(self):
+        """
+        Ensure utils method `api_get_token` works and
+        return token.
+        """
+        first_token = api_get_token()
+        self.assertIsInstance(first_token, str)
+
+        second_token = api_get_token()
+        self.assertIsInstance(second_token, str)
+
+        self.assertNotEqual(first_token, second_token)
+        self.assertEqual(len(first_token), len(second_token))
+
+    def test_utils_api_get_images_pages_count_works(self):
+        """
+        Ensure utils method `api_get_images_page_count` works
+        and return valid count of pages.
+        """
+
+        pages_count = api_get_images_pages_count()
+        self.assertIsInstance(pages_count, int)
+        self.assertNotEqual(pages_count, 0)
+
+    def test_utils_api_get_images_id_works(self):
+        """
+        Ensure utils method 'api_get_images_id' works and
+        return valid list of pictures id.
+        """
+        images_count = api_get_images_pages_count() * 10
+        images_id_list = api_get_images_id()
+
+        self.assertIsInstance(images_id_list, list)
+        self.assertEqual(len(images_id_list), images_count)
+
+    def test_utils_build_images_urls_works(self):
+        """
+        Ensure utils method `build_images_urls` works and
+        return valid list of urls.
+        """
+        images_id_list = api_get_images_id()
+        images_urls_list = build_images_urls()
+
+        self.assertIsInstance(images_urls_list, list)
+        self.assertIsInstance(images_id_list, list)
+        self.assertEqual(len(images_id_list), len(images_urls_list))
---
+++ gallery/utils.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+import requests
+
+API_KEY = os.environ['API_KEY']
+
+
+def api_get_token():
+    """Authorize on external API and return access token."""
+    url = 'http://interview.agileengine.com/auth'
+    payload = {
+        'apiKey': API_KEY
+    }
+
+    response = requests.post(url, json=payload)
+    token = json.loads(response.text)['token']
+
+    return token
+
+
+def api_get_images_pages_count():
+    """Call external API ang return total pages count value."""
+    url = 'http://interview.agileengine.com/images'
+    header = {
+        'Authorization': 'Bearer {}'.format(api_get_token())
+    }
+
+    response = requests.get(url, headers=header)
+    pages_count = int(json.loads(response.text)['pageCount'])
+
+    return pages_count
+
+
+def api_get_images_id():
+    """Call external API and return list with pictures id."""
+    data = []
+    pages = api_get_images_pages_count()
+
+    for page in range(pages):
+        url = 'http://interview.agileengine.com/images?page={}'.format(page)
+        header = {
+            'Authorization': 'Bearer {}'.format(api_get_token())
+        }
+        response = requests.get(url, headers=header)
+        pictures = json.loads(response.text)['pictures']
+
+        for picture in pictures:
+            data.append(picture['id'])
+
+    return data
+
+
+def build_images_urls():
+    """Build and return list of urls from pictures id."""
+    data = []
+
+    for picture_id in api_get_images_id():
+        picture_url = 'http://interview.agileengine.com/images/{}'.format(picture_id)
+        data.append(picture_url)
+
+    return data
